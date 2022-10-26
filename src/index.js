a5cd4086b0187ee7f40f8e94470a1b6de510522c
Beau Lazear <beaulazear@gmail.com>
2022-10-25 20:00:08 -0400

    commit tree f99e0059e39bc6db4b83ffafe210609f4f4121f2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+let breedNames = [];
+let userLetter = 'a';
+
+document.addEventListener('DOMContentLoaded', () => {
+   fetch("https://dog.ceo/api/breeds/image/random")
+    .then(resp => resp.json())
+    .then(json => {
+        document.getElementById("imageTag").src = json.message;
+    })
+
+    document.getElementById("breed-dropdown").onchange = (event) => {
+        // getting the user selected letter value
+        userLetter = event.target.value;
+        setBreedsOnPage();
+    }
+})
+
+
+fetch('https://dog.ceo/api/breeds/list/all')
+    .then(resp => resp.json())
+    .then(json => {
+        // object destructuring
+        const { message } = json;
+
+        // get keys from object ['affenpincher', 'boxer', ...]
+        breedNames = Object.keys(message);
+ 
+
+    })
+
+const setBreedsOnPage = () => {
+    // filter out all breeds that don't have userLetter as the first character and map to return the name in a list item
+    const filteredBreedNames = breedNames.filter((val) => val.charAt(0) === userLetter).map((name) => `<li>${name}</li>`);
+    // get the list
+    const list = document.querySelector("ul");
+    // replace innerHtml with filtered breednames
+    list.innerHTML = filteredBreedNames.join(' ');
+}
+
+function changeColor() {
+    document.querySelector('li').addEventListener(`click`, e)
+    console.log(e);
+}
